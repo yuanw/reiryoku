93988c7e26d39afb2424085c7111ffea0ad3d263
yuanw <me@yuanwang.ca>
2023-03-11 06:15:51 -0600

    wip

--- flake.nix
+++ flake.nix
@@ -48,7 +48,6 @@
           '';
 
           buildPhase = ''
-            ls  ./keyboards/bastardkb/charybdis/3x5/keymaps
             make bastardkb/charybdis/3x5/v2/splinky_3:yuanw SKIP_GIT=1
             ${pkgs.qmk}/bin/qmk  -v c2json -kb bastardkb/charybdis/3x5/v2/splinky_3 -km yuanw ./keyboards/bastardkb/charybdis/3x5/keymaps/yuanw/keymap.c > reiryoku.json
             ${drawer}/bin/keymap parse -c 10 -q reiryoku.json  > reiryoku.yaml
