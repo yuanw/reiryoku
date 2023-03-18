d22a84cac3cda184604f6940ab511567aa2a5c59
yuanwang <yuan.wang@workiva.com>
2023-03-18 13:15:45 -0600

    fix script

--- flake.nix
+++ flake.nix
@@ -72,7 +72,7 @@
 
         flash = pkgs.writeScriptBin "reiryoku-flash" ''
           cd ${qmk_firmware}
-          ${pkgs.qmk}/bin/qmk flash ${firmware}/bastardkb_charybdis_3x5_v2_splinky_3_yuanw.uf2
+          ${pkgs.qmk}/bin/qmk flash ${firmware}/share/bastardkb_charybdis_3x5_v2_splinky_3_yuanw.uf2
         '';
 
       in rec {
@@ -81,10 +81,12 @@
           paths = [ firmware flash ];
         };
         packages.drawer = drawer;
-        packages.flash = flash;
         apps.flash = {
           type = "app";
-          program = "${packages.flash}";
+          program = (pkgs.writeScript "qmk-flash" ''
+            cd ${qmk_firmware}
+            ${pkgs.qmk}/bin/qmk flash ${packages.firmware}/share/bastardkb_charybdis_3x5_v2_splinky_3_yuanw.uf2
+          '');
         };
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
