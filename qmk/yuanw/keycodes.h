c032dfefef8d5efcf7fc24b254ba1896a126401c
yuanwang <yuan.wang@workiva.com>
2024-01-01 06:47:42 -0600

    wip

--- qmk/yuanw/keycodes.h
+++ qmk/yuanw/keycodes.h
@@ -20,4 +20,10 @@ enum charybdis_keymap_layers {
 #define _L_PTR(KC) LT(LAYER_POINTER, KC)
 
 
-enum my_keycodes { RDO = SAFE_RANGE, PST, CPY, CUT, UND,REPEAT, REV_REP, };
+enum my_keycodes { RDO = SAFE_RANGE,
+                   PST,
+                   CPY,
+                   CUT,
+                   UND,
+                   REPEAT,
+                   REV_REP };
