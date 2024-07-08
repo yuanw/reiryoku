0827a1d08bc44fe54e419259d371b3ae2adcb77b
yuanwang <yuan.wang@workiva.com>
2024-07-08 09:36:51 -0600

    move src at top

--- qmk/yuanw/rules.mk
+++ qmk/yuanw/rules.mk
@@ -1,8 +1,8 @@
+SRC += features/custom_shift_keys.c
+
 # VIA_ENABLE = yes
 COMBO_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 OS_DETECTION_ENABLE = yes
 REPEAT_KEY_ENABLE = yes
 SEND_STRING_ENABLE = yes
-
-SRC += features/custom_shift_keys.c
